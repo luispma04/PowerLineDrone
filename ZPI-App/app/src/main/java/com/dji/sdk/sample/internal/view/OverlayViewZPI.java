720eccafdced6fa6ce8fc092c80085270f3081c3
Kamil Taras <116663154+KamilTaras@users.noreply.github.com>
2024-11-21 18:10:09 +0100

    style: making CEP more appealing (#11)

--- ZPI-App/app/src/main/java/com/dji/sdk/sample/internal/view/OverlayViewZPI.java
+++ ZPI-App/app/src/main/java/com/dji/sdk/sample/internal/view/OverlayViewZPI.java
@@ -3,6 +3,7 @@ package com.dji.sdk.sample.internal.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -15,6 +16,7 @@ public class OverlayViewZPI extends View {
     private Paint paint99;
     private boolean showCircles = false; // Circles are hidden by default
 
+    private int OPACITY = 90;
     public OverlayViewZPI(Context context) {
         super(context);
         init();
@@ -28,18 +30,18 @@ public class OverlayViewZPI extends View {
     private void init() {
         paint50 = new Paint();
         paint50.setColor(getResources().getColor(android.R.color.holo_red_light));
-        paint50.setStyle(Paint.Style.STROKE);
-        paint50.setStrokeWidth(5);
+        paint50.setStyle(Paint.Style.FILL);
+        paint50.setAlpha(OPACITY); // 50% opacity
 
         paint93 = new Paint();
         paint93.setColor(getResources().getColor(android.R.color.holo_orange_light));
-        paint93.setStyle(Paint.Style.STROKE);
-        paint93.setStrokeWidth(5);
+        paint93.setStyle(Paint.Style.FILL);
+        paint93.setAlpha(OPACITY);
 
         paint99 = new Paint();
         paint99.setColor(getResources().getColor(android.R.color.holo_green_light));
-        paint99.setStyle(Paint.Style.STROKE);
-        paint99.setStrokeWidth(5);
+        paint99.setStyle(Paint.Style.FILL);
+        paint99.setAlpha(OPACITY);
     }
 
     public void updateRadii(float radius50, float radius93, float radius99) {
@@ -69,15 +71,25 @@ public class OverlayViewZPI extends View {
         float centerX = getWidth() / 2f;
         float centerY = getHeight() / 2f;
 
-        // Draw the circles if radii are greater than zero
+        // Draw the innermost circle (red)
         if (radius50 > 0) {
             canvas.drawCircle(centerX, centerY, radius50, paint50);
         }
-        if (radius93 > 0) {
-            canvas.drawCircle(centerX, centerY, radius93, paint93);
+
+        // Draw the middle ring (orange)
+        if (radius93 > radius50) {
+            Path ringPath93 = new Path();
+            ringPath93.addCircle(centerX, centerY, radius93, Path.Direction.CW);
+            ringPath93.addCircle(centerX, centerY, radius50, Path.Direction.CCW);
+            canvas.drawPath(ringPath93, paint93);
         }
-        if (radius99 > 0) {
-            canvas.drawCircle(centerX, centerY, radius99, paint99);
+
+        // Draw the outer ring (green)
+        if (radius99 > radius93) {
+            Path ringPath99 = new Path();
+            ringPath99.addCircle(centerX, centerY, radius99, Path.Direction.CW);
+            ringPath99.addCircle(centerX, centerY, radius93, Path.Direction.CCW);
+            canvas.drawPath(ringPath99, paint99);
         }
     }
 }
